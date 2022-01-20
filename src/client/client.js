0ce4dcd7b22320c98ccc6986a2ac317d7e443bd3
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2022-01-20 15:17:51 -0300

    adicionando script de start

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-console.log(process.env.BOT_TOKEN);
 client.login(process.env.BOT_TOKEN);
 client.once('ready', () => console.log(`Bot ${client.user.username} is now online`));
 
